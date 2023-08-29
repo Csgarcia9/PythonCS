58620c8ae16e200a405c3ddd0efc29925807ea34
Csgarcia9 <122550982+Csgarcia9@users.noreply.github.com>
2023-08-28 22:10:53 -0400

    Create Facturaelectrica.py

---
+++ Facturaelectrica.py
@@ -0,0 +1,16 @@
+def factura_luz(consumo):
+    
+    if consumo <= 200:
+        factura = consumo * 5
+        return factura 
+    elif consumo <= 290:
+        factura = ((consumo - 200)* 8) + (200 * 5)
+        return factura 
+    elif consumo > 290:
+        restante1 = 90 * 8
+        restante2 = (consumo - 290) * 11
+        factura = restante1 + restante2 + (200 * 5)
+        return factura 
+        
+consumo = float(input("Digite el consumo electrico: "))  
+print("Su factura es de: ",factura_luz(consumo), "Pesos")
